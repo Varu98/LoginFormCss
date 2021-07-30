c0b46f98684d4747e62cfa602bd261d918f03f0c
Vardaan Agarwal <69509962+Varu98@users.noreply.github.com>
2021-07-30 22:52:12 +0530

    css positioning fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ import Input from "./Input";
 export default function App() {
   return (
     <div className="App">
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
       <Input isLoggedIn="true" />
     </div>
   );
