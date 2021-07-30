0d13807970459040056666a17b363a48a0b2e031
Vardaan Agarwal <69509962+Varu98@users.noreply.github.com>
2021-07-30 22:41:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import Input from "./Input";
+
+export default function App() {
+  return (
+    <div className="App">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Input isLoggedIn="true" />
+    </div>
+  );
+}
---
+++ src/Input.jsx
@@ -0,0 +1,14 @@
+const Import = (props) => {
+  return (
+    <div>
+      <form id="form">
+        <input placeholder="Username" type="text" />
+        <br />
+        <input placeholder="Password" type="text" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+export default Import;
